cf337364cf175a1d0ee4d543d0a192d2edeab0bd
Konstantin Alikhanov <atomAltera@gmail.com>
2013-07-30 21:23:11 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fcchelsea::Application.config.secret_key_base = 'a8f5829b3b0580ed5c0c75901c4f109d491e56964d98bcad916b2bf4652451fec16b211e2373ab31d72578014992ca16e4e7dc4a0851e118deb9fd59f7c1a202'
